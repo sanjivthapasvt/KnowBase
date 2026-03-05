4e5df963b0a3b8fd753c501aeb843d63c8fbdd29
Sanjiv Thapa <thapasvt12@gmail.com>
2026-03-05 08:51:18 +0545

    ✅ Updated main api router to include new routers

--- app/api/router.py
+++ app/api/router.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
 
 from app.modules.auth.router import router as auth_router
-
+from app.modules.memberships.router import router as memberships_router
+from app.modules.organizations.router import router as organizations_router
 from app.modules.users.router import router as users_router
 
 api_router = APIRouter()
@@ -11,3 +12,5 @@ api_router.include_router(auth_router)
 
 # Authenticated routes
 api_router.include_router(users_router)
+api_router.include_router(organizations_router)
+api_router.include_router(memberships_router)
